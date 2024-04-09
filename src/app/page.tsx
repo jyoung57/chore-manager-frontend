5f8aa01ae069c9d9973791bea267f7d30743c153
Jon <jyoung57@W-BC91R73>
2024-04-08 21:10:27 -0500

    added axios for api test call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
 import Image from 'next/image';
+import axios from 'axios';
 
 export default function Home() {
+  const baseURL: string | undefined = process.env.NEXT_PUBLIC_SERVER;
+  if (baseURL) {
+    axios.get(baseURL)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }
+
   return (
     <>
       <header>
