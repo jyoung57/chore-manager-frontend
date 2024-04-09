cfaf9d1ead7477aa730b91e55556beb090ffce3e
Jon <jyoung57@W-BC91R73>
2024-04-08 20:09:54 -0500

    added About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// @ts-ignore
+function page(props) {
+  return (
+    <>
+      <h1>About Chore Manager</h1>
+    </>
+  );
+}
+
+page.propTypes = {}
+
+export default page
